0de2b2fb3afb52a4e038e72fb14a2400b54e0383
DeadKn1ght <deadkn1ght@bk.ru>
2022-11-30 15:47:42 +0300

    Added task #10 fron Homework

---
+++ task#10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+Console.WriteLine("Input three-digit number :");
+int number = int.Parse(Console.ReadLine());
+if ((99 < number) && (number < 1000))
+    Console.WriteLine("Second digit is :" + (number / 10) % 10);
+else
+    Console.WriteLine("You input incorect number");
